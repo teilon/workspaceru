63890fc562e7088397285ff7fd5268921701e76a
teilon <teiloncw@gmail.com>
2021-03-10 23:49:38 +0600

    add lua script load like landing page with button

--- workspaceru/workspaceru/settings.py
+++ workspaceru/workspaceru/settings.py
@@ -44,15 +44,19 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
-#SPIDER_MIDDLEWARES = {
-#    'workspaceru.middlewares.WorkspaceruSpiderMiddleware': 543,
-#}
+SPIDER_MIDDLEWARES = {
+    'scrapy_splash.SplashDeduplicateArgsMiddleware': 100,
+}
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'workspaceru.middlewares.WorkspaceruDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'scrapy_splash.SplashCookiesMiddleware': 723,
+    'scrapy_splash.SplashMiddleware': 725,
+    'scrapy.downloadermiddlewares.httpcompression.HttpCompressionMiddleware': 810,
+}
+
+DUPEFILTER_CLASS = 'scrapy_splash.SplashAwareDupeFilter'
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
@@ -86,3 +90,7 @@ ROBOTSTXT_OBEY = True
 #HTTPCACHE_DIR = 'httpcache'
 #HTTPCACHE_IGNORE_HTTP_CODES = []
 #HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
+FEED_EXPORT_ENCODING = 'utf-8'
+
+SPLASH_URL = 'http://localhost:8050'
--- workspaceru/workspaceru/spiders/workspace_crawler.py
+++ workspaceru/workspaceru/spiders/workspace_crawler.py
@@ -1,10 +1,87 @@
 import scrapy
+from scrapy_splash import SplashRequest
 
 
 class WorkspaceCrawlerSpider(scrapy.Spider):
     name = 'workspace_crawler'
     allowed_domains = ['workspace.ru']
-    start_urls = ['https://workspace.ru/contractors']
+    #start_urls = ['https://workspace.ru/contractors']
+
+    show_categories = '''
+        function main(splash, args)
+            splash:on_request(function(request)
+                request:set_header('User-Agent', "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0")
+            end)
+
+            url = args.url
+            assert(splash:go(url))
+            assert(splash:wait(1))
+            splash:set_viewport_full()
+            return splash:html()
+        end    
+    '''
+    load_more = '''
+        function main(splash, args)
+            splash.private_mode_enabled = false
+  
+	        splash:on_request(function(request)
+  	            request:set_header('User-Agent', "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:85.0) Gecko/20100101 Firefox/85.0")
+            end)
+
+            url = args.url
+            assert(splash:go(url))
+            assert(splash:wait(1))
+    
+            while (true)
+            do
+                stoped = splash:select(".alert"and".alert-success"and".green-success")
+		        if (stoped~=nil)
+                then
+                    break
+                end
+    
+                a_more = assert(splash:select("#loadMore"))
+                a_more:mouse_click()
+                assert(splash:wait(1))
+            end
+
+            splash:set_viewport_full()
+            return splash:html()
+        end  
+    '''
+    
+    # <a 
+    #     href="javascript:void(0)" 
+    #     class="box-more__btn" 
+    #     id="loadMore" 
+    #     data-service-code="crm"
+    #     >
+    # Загрузить ещё
+    # </a>
+
+    # <a 
+    #     href="javascript:void(0)" 
+    #     class="box-more__btn" 
+    #     id="loadMore" 
+    #     data-service-code="crm" 
+    #     style="display: none;"
+    #     >
+    # Загрузить ещё
+    # </a>
+
+    # <div 
+    #     class="alert alert-success green-success"
+    #     >
+    # Все загрузили
+    # </div>
+
+    def start_requests(self):
+        yield SplashRequest(url="https://workspace.ru/contractors",
+                            callback=self.parse,
+                            endpoint='execute',
+                            args={
+                                'lua_source': self.show_categories
+                            })
 
     def parse(self, response):
         categories = response.xpath("//div[@class='categories__item']//div[@class='categories__card']")
@@ -13,15 +90,16 @@ class WorkspaceCrawlerSpider(scrapy.Spider):
             
             cats = category.xpath(".//ul[@class='categories__card-list']/li")
             for cat in cats:
-                link = cat.xpath("/a/@href").get()
+                link = cat.xpath(".//a/@href").get()
+                link_name = cat.xpath(".//a/text()").get()
 
-                link_name = cat.xpath("/a/text()").get()
-                yield {
-                    'cat': category_name,
-                    'link': link_name
-                }
+                # yield {
+                #     'category_name': category_name,
+                #     'link_name': link_name,
+                #     'link': link,
+                # }
 
-                # yield response.follow(url=link, callback=self.parse_cats, meta={'category_name': category_name})
+                yield response.follow(url=link, callback=self.parse_cats, meta={'category_name': category_name})
     
     def parse_cats(self, responce):
         pass
